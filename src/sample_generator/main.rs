bcf4cd033dcce68e165ed5b0b92d2682f0258823
Raja Farooq <96690967+raja-sahab@users.noreply.github.com>
2023-10-22 19:45:06 +0200

    Update main.rs

--- src/sample_generator/main.rs
+++ src/sample_generator/main.rs
@@ -4,6 +4,8 @@ use ron::de::from_reader;
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
 use std::path::Path;
+//use serde_derive::Deserialize;
+use ron;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -12,29 +14,34 @@ struct Args {
 
     #[clap(short = 'f', long, default_value = "sample.ron")]
     sample_file: String, //taking command line argument for the sample file
+
+    #[arg(short, long, default_value_t = false)]
+    multithread: bool,
 }
+
 const N: usize = 2; // number of propositional variables
 
 fn evaluate_formulas(
+    contents: &[u8],
+    multithread: bool,
     formulas: &[SyntaxTree],
     sample: &Sample<N>,
-)
-{
+) -> Option<String> {
     for (i, formula) in formulas.iter().enumerate() {
         let mut positive_count = 0;
         let mut negative_count = 0;
 
-        // Count how many positive traces each formula satisfies
-        for trace in &sample.positive_traces {
-            if sample.is_consistent(formula) {
-                positive_count += 1;
-            }
-        }
-
-        // Count how many negative traces each formula satisfies
-        for trace in &sample.negative_traces {
-            if !sample.is_consistent(formula) {
-                negative_count += 1;
+        for content in contents.chunks(10) {
+            // Attempt to deserialize the content
+            if let Ok(deserialized_sample) = ron::de::from_bytes::<Sample<N>>(content) {
+                // Check if the deserialized sample is consistent with the formula
+                if deserialized_sample.is_consistent(formula) {
+                    // If consistent, increment the positive count
+                    positive_count += 1;
+                } else {
+                    // Otherwise, increment the negative count
+                    negative_count += 1;
+                }
             }
         }
 
@@ -46,15 +53,18 @@ fn evaluate_formulas(
             i, positive_count, negative_count, fitness
         );
     }
+
+    // Return
+    Some("Result string".to_string())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    //const N: usize = 2; // number of propositional variables
+    let multithread: bool = true; // Initialize multithread with a value
     let size = args.size; // size of the formula
 
-    let vars = vec![0; N - 1];
+    let vars = vec![0, N-1];
 
     // Convert Vec<i32> into Vec<u8>
     let vars_vec: Vec<u8> = vars.iter().map(|&x| x as u8).collect();
@@ -72,16 +82,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // Deserialize the sample of traces from a .ron file
+    //let path = Path::new(&args.sample);
     let sample_filename = &args.sample_file;
     let file = File::open(sample_filename)?;
     let mut buf_reader = BufReader::new(file);
-    let mut sample_content = String::new();
-    buf_reader.read_to_string(&mut sample_content)?;
+    let mut content = Vec::new();
+    buf_reader.read_to_end(&mut content)?;
 
-    let sample: Sample<N> = from_reader(sample_content.as_bytes())?;
+    let sample: Sample<N> = from_reader(&content[..])?;
 
     // Evaluate formulas
-    evaluate_formulas(&formulas, &sample);
+    evaluate_formulas(&content, multithread, &formulas, &sample);
 
     // Saving the list of formulas in a txt file
     let filename = "formulas.txt";
